c22b90bbb04411533b8c9eb35b85b9ff22512ffe
Roman Vogel <vogel.roman@gmail.com>
2019-12-21 14:06:34 +0100

    ..

--- WordClock/ColorSequencer.h
+++ WordClock/ColorSequencer.h
@@ -8,7 +8,7 @@
 class ColorSequencer
 {
 	public:
-		ColorSequencer(unsigned long speed=1000, unsigned short maxBrightness=255, int minBrightnessSum=25);
+		ColorSequencer(unsigned long speed=1000, unsigned short maxBrightness=255, int minBrightnessSum=75);
 		void call(int ldr=512, bool init=false);
 		void getNew();
 		float ldrCalc = 0.5;
